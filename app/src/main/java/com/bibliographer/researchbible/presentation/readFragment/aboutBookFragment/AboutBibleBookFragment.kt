ede6137daebce13035c494b0178c9155daf91eb9
elsloude <elisey.vavulin15@gmail.com>
2022-10-31 16:22:26 +0100

    what new...

---
+++ app/src/main/java/com/bibliographer/researchbible/presentation/readFragment/aboutBookFragment/AboutBibleBookFragment.kt
@@ -0,0 +1,38 @@
+package com.bibliographer.researchbible.presentation.readFragment.aboutBookFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bibliographer.researchbible.R
+
+class AboutBibleBookFragment : Fragment() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_about_bible_book, container, false)
+    }
+
+    companion object {
+
+        const val AAA = "чет новое"
+
+        @JvmStatic
+        fun newInstance() =
+            AboutBibleBookFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
